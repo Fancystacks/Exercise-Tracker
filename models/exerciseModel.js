548cac3cf6e897df9f2e3b5f76509b9aecb328c8
Leah Migacz <lmigacz@msudenver.edu>
2020-02-15 19:51:56 -0700

    got workouts to complete

--- models/exerciseModel.js
+++ models/exerciseModel.js
@@ -7,7 +7,7 @@ const WorkoutSchema = new Schema ({
         type: Date,
         default: Date.now
     },
-    workouts : [
+    exercises : [
         {
             type: {
                 type: String,
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -22,15 +22,17 @@ router.get("/api/workouts/range", (req, res) => {
     });
 });
 
-router.put("/api/workouts/:id", (req, res) => {
-    Workout.findByIDAndUpdate(req.params.id, { $push: { exercises: req.body } }, { new: true })
-    .then(data => {
-      res.json(data);
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(params.id, {
+      $push: { exercises: body },
     })
-    .catch(err => {
-      res.json(err);
-    });
-});
+    .then(data => {
+        res.json(data);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
 
 router.post("/api/workouts", (req, res) => {
     Workout.create(req.body)
